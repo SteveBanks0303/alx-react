1010cfa45698fc86b1c8dc540ec3ed7e1fce750a
SteveBanks0303 <stephenbankole03@gmail.com>
2023-07-28 01:50:30 +0000

    hmmmn

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,13 @@
+// src/index.js
+// import $ from 'jquery';
+//
+// $(document).ready(function () {
+//   // Create three paragraphs
+//     const paragraph1 = $('<p>Holberton Dashboard</p>');
+//       const paragraph2 = $('<p>Dashboard data for the students</p>');
+//         const paragraph3 = $('<p>Copyright - Holberton School</p>');
+//
+//           // Append paragraphs to the body
+//             $('body').append(paragraph1, paragraph2, paragraph3);
+//             });
+//
